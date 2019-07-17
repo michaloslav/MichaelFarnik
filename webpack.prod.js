2d89ae3898a28870796e4d94683a68fe3c81c36b
michaloslav <31165844+michaloslav@users.noreply.github.com>
2019-07-17 23:11:02 +0200

    Added CNAME

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
 const merge = require("webpack-merge")
 const common = require('./webpack.common.js')
+const CnameWebpackPlugin = require('cname-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new CnameWebpackPlugin({
+      domain: 'michaelfarnik.js.org',
+    }),
+  ],
 })
