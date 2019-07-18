cd4caf0c51ba0cbbc0e732857f711599338dbafd
michaloslav <31165844+michaloslav@users.noreply.github.com>
2019-07-18 02:29:27 +0200

    Babel config fix

--- webpack.common.js
+++ webpack.common.js
@@ -66,7 +66,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"]
+        }
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
